883719c9c70235f831e5070dece89d946a0be74e
Loikiy <danielsavalalhoretti@gmail.com>
2024-05-02 02:14:57 -0300

    Feat: new "SignIn" screen

---
+++ Aplicativo/src/constants/styleSignIn.js
@@ -0,0 +1,126 @@
+import { StyleSheet } from 'react-native';
+import { width, height} from './dimensions';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  header: {
+    width: '100%',
+    height: '30%',
+    paddingHorizontal: '10%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerText: {
+    color: '#162b9e',
+    fontSize: 80,
+    textAlign: 'center',
+  },
+  subHeaderText: {
+    color: '#162b9e',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  signIn: {
+    width: '100%',
+    height: '50%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    width: '80%',
+    height: '15%',
+    borderRadius: 25,
+    borderWidth: 2,
+    borderBlockColor: '#000',
+    color: '#162b9e',
+    fontSize: 20,
+    fontWeight: 'bold',
+    paddingLeft: '2%',
+    backgroundColor: '#fff',
+  },
+  forgotButton: {
+    width: '80%',
+    height: 'auto',
+  },
+  forgotButtonText: {
+    color: '#162b9e',
+    fontSize: 20,
+  },
+  buttonSignIn: {
+    width: '80%',
+    height: '15%',
+    borderRadius: 25,
+    borderWidth: 2,
+    borderColor: '#162b9e',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#162b9e',
+  },
+  buttonSignInText: {
+    color: '#fff',
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  buttonBack: {
+    width: '40%',
+    height: '15%',
+    borderRadius: 25,
+    borderWidth: 2,
+    borderColor: '#162b9e',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#162b9e',
+  },
+  buttonBackText: {
+    color: '#fff',
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  buttonBackgroundImage: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    borderRadius: 25,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  changeAccess: {
+    width: '100%',
+    height: '15%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  changeAccessText: {
+    color: '#162b9e',
+    fontSize: 25,
+  },
+  navButtonSignUp: {
+    width: 'auto',
+    height: 'auto',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  navButtonSignUpText: {
+    color: '#162b9e',
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  footer: {
+    width: '100%',
+    height: '5%',
+    position: 'relative',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#162b9e',
+  },
+});
+
+export default styles;
---
+++ Aplicativo/src/screens/SignIn/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styles from '../../constants/styleSignIn';
+import { SafeAreaView, View, Text, TextInput, Pressable, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+export default function SignIn() {
+    const navigation = useNavigation();
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.headerText}>GyMate</Text>
+                <Text style={styles.subHeaderText}>coloque seus dados de acesso para iniciar sua sessão</Text>
+            </View>
+
+            <View style={styles.signIn}>
+                <TextInput style={styles.input} placeholder='Digite seu e-mail' keyboardType='email-address'/>
+                <br></br>
+                <TextInput style={styles.input} placeholder='Digite sua senha' secureTextEntry={true}/>
+
+                <Pressable style={styles.forgotButton}>
+                    <Text style={styles.forgotButtonText}>Esqueci minha senha</Text>
+                </Pressable>
+                <br></br>
+                <Pressable style={styles.buttonSignIn}>
+                    <Image style={styles.buttonBackgroundImage} source={require('../../assets/Fundo-GyMate-90º.png')}/>
+                    <Text style={styles.buttonSignInText}>ENTRAR</Text>
+                </Pressable>
+                <br></br>
+                <Pressable style={styles.buttonBack} onPress={ () => navigation.navigate('GyMate')}>
+                    <Text style={styles.buttonBackText}>VOLTAR</Text>
+                </Pressable>
+            </View>
+
+            <View style={styles.changeAccess}>
+                <Text style={styles.changeAccessText}>Ainda não possui uma conta?</Text>
+                <Pressable style={styles.navButtonSignUp} onPress={ () => navigation.navigate('GyMate Sign-up')}>
+                    <Text style={styles.navButtonSignUpText}>CLIQUE AQUI</Text>
+                </Pressable>
+            </View>
+
+            <View style={styles.footer}/>
+        </SafeAreaView>
+    );
+}
